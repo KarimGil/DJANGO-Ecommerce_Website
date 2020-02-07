ca78f277a1da60f5c0e9f55e8b1fdf8ea7905c86
KarimGil <karimg87@hotmail.com>
2020-02-07 08:56:36 -0800

    Working

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
+    path('', views.home_page, name='home_page'),
     path('product_page', views.product_page, name='product_page'),
     path('checkout_page', views.checkout_page, name='checkout_page'),
 
--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from .models import Item
 # Create your views here.
 
 
-def item_list(request):
+def home_page(request):
 
     context = {
 
